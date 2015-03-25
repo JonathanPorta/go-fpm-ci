5e8881b487d51b32831887464a429478e6c96709
Jonathan Porta <jonathan@jonathanPorta.com>
2015-03-25 08:41:33 -0700

    chmod scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,15 +2,15 @@
 
 # We have to build go 1.5 if we want to cross compile
 # Kind of messy, but will be a lot better when 1.5 is released!
-source ./scripts/golang.sh
+source $(pwd)/scripts/golang.sh
 
 # Actually build our app
-source ./scripts/config.sh
+source $(pwd)/scripts/config.sh
 
 for TARGET in "${!TARGETS[@]}"
 do
   echo "Building for $TARGET...\n\n"
-  mkdir -p ./build/$TARGET
+  mkdir -p $(pwd)/build/$TARGET
   echo "GOOS=$TARGET GOARCH=${TARGETS[$TARGET]} $GOBUILD build -o ./build/$TARGET/${EXECUTABLES[$TARGET]} ./src/hello.go"
   GOOS=$TARGET GOARCH=${TARGETS[$TARGET]} $GOBUILD build -o ./build/$TARGET/${EXECUTABLES[$TARGET]} ./src/hello.go
 done

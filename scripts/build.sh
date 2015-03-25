cf2fd197f2949b9291c22308fa1d117238051e32
Jonathan Porta <jonathan@jonathanPorta.com>
2015-03-25 09:02:56 -0700

    updates

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,7 @@
 
 # We have to build go 1.5 if we want to cross compile
 # Kind of messy, but will be a lot better when 1.5 is released!
-source $(pwd)/scripts/golang.sh
+# source $(pwd)/scripts/golang.sh
 
 # Actually build our app
 source $(pwd)/scripts/config.sh
@@ -11,6 +11,6 @@ for TARGET in "${!TARGETS[@]}"
 do
   echo "Building for $TARGET...\n\n"
   mkdir -p $(pwd)/build/$TARGET
-  echo "GOOS=$TARGET GOARCH=${TARGETS[$TARGET]} $GOBUILD build -o ./build/$TARGET/${EXECUTABLES[$TARGET]} ./src/hello.go"
-  GOOS=$TARGET GOARCH=${TARGETS[$TARGET]} $GOBUILD build -o ./build/$TARGET/${EXECUTABLES[$TARGET]} ./src/hello.go
+  echo "GOOS=$TARGET GOARCH=${TARGETS[$TARGET]} go build -o $(pwd)/build/$TARGET/${EXECUTABLES[$TARGET]} $(pwd)/src/hello.go"
+  GOOS=$TARGET GOARCH=${TARGETS[$TARGET]} go build -o $(pwd)/build/$TARGET/${EXECUTABLES[$TARGET]} $(pwd)/src/hello.go
 done

1cee1f4914158a26bf336b32f9caf0552fcc7bf4
Jonathan Porta <jonathan@jonathanPorta.com>
2015-03-25 18:26:50 -0700

    meh

--- scripts/golang.sh
+++ scripts/golang.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+START_DIR=$(pwd)
+echo "Starting in $START_DIR..."
 
 git clone https://go.googlesource.com/go $HOME/go1.4
 cd $HOME/go1.4/src
@@ -8,3 +10,7 @@ git checkout release-branch.go1.4
 git clone https://go.googlesource.com/go $HOME/go
 cd $HOME/go/src
 GOROOT_BOOTSTRAP=$HOME/go1.4 ./all.bash
+
+echo "Finished in $(pwd)"
+echo "Returning to $START_DIR"
+cd $START_DIR

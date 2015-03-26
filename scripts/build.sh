75307aaf04bcade9d55b01f2a10636b432e1a6e3
Jonathan Porta <jonathan@jonathanPorta.com>
2015-03-26 09:15:51 -0700

    meh

--- scripts/build.sh
+++ scripts/build.sh
@@ -18,6 +18,8 @@ for TARGET in "${!TARGETS[@]}"
 do
   echo -e "\n\nBuilding for $TARGET..."
   mkdir -p $(pwd)/build/$TARGET
-  echo "GOOS=$TARGET GOARCH=${TARGETS[$TARGET]} $HOME/go/bin/go build -o $(pwd)/build/$TARGET/${EXECUTABLES[$TARGET]} $(pwd)/src/hello.go"
-  GOOS=$TARGET GOARCH=${TARGETS[$TARGET]} $HOME/go/bin/go build -o $(pwd)/build/$TARGET/${EXECUTABLES[$TARGET]} $(pwd)/src/hello.go
+  echo "GOOS=$TARGET GOARCH=${TARGETS[$TARGET]} go build -o $(pwd)/build/$TARGET/${EXECUTABLES[$TARGET]} $(pwd)/src/hello.go"
+  GOOS=$TARGET GOARCH=${TARGETS[$TARGET]} go build -o $(pwd)/build/$TARGET/${EXECUTABLES[$TARGET]} $(pwd)/src/hello.go
+  # echo "GOOS=$TARGET GOARCH=${TARGETS[$TARGET]} $HOME/go/bin/go build -o $(pwd)/build/$TARGET/${EXECUTABLES[$TARGET]} $(pwd)/src/hello.go"
+  # GOOS=$TARGET GOARCH=${TARGETS[$TARGET]} $HOME/go/bin/go build -o $(pwd)/build/$TARGET/${EXECUTABLES[$TARGET]} $(pwd)/src/hello.go
 done
